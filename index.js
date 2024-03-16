bc99591ee07dee45782edae9de8af2ff0a1cc822
rimasultana <rimasultana20225@gmail.com>
2024-03-16 07:50:13 +0600

    constractor function

---
+++ index.js
@@ -0,0 +1,11 @@
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+  
+  // Create a Person object
+  const myFather = new Person("John", "Doe", 50, "blue");
+  
+console.log(myFather);
